de321806e442cfad952afc5c72389acae0b69067
miludedeng <383252209@qq.com>
2016-04-16 21:38:27 +0800

    添加多线程

--- controllers/default.go
+++ controllers/default.go
@@ -35,18 +35,15 @@ func (c *MainController) Get() {
 	expDateInt64 := int64(expDateInt)
 	url := c.Ctx.Request.URL.String()
 	domain := beego.AppConfig.String("app_domain")
+	fullUrl := domain + url
 	if strings.HasSuffix(domain, "/") {
 		domain = domain[0 : len(domain)-1]
 	}
-	md5Ctx := md5.New()
-	md5Ctx.Write([]byte(string(url)))
-	cipherEncode := md5Ctx.Sum(nil)
-	cipherStr := hex.EncodeToString(cipherEncode)
+	cipherStr := EncodeUrl(url)
 	fileName := cipherStr + ".html"
-	folder1 := fileName[0:1]
-	folder2 := fileName[1:2]
-	abPath := staticPath + "/" + folder1 + "/" + folder2 + "/" + fileName
-	if isExist(abPath) {
+	abDir := staticPath + "/" + fileName[0:1] + "/" + fileName[1:2]
+	abPath := abDir + "/" + fileName
+	if IsExist(abPath) {
 		file, err1 := os.Open(abPath)
 		fileState, _ := os.Stat(abPath)
 		timeDifference := time.Now().Unix() - fileState.ModTime().Unix()
@@ -59,33 +56,41 @@ func (c *MainController) Get() {
 		html := string(fd)
 		c.Ctx.WriteString(html)
 		useOldPage = true
-		if timeDifference <= expDateInt64 || ContainsBySlice(md5S, cipherStr) {
+		if timeDifference < expDateInt64 || ContainsBySlice(md5S, cipherStr) {
 			return
 		}
 	}
 	if !useOldPage && ContainsBySlice(md5S, cipherStr) {
-		fmt.Println(2, md5S)
 		c.Ctx.WriteString("<script>setTimeout('location.reload()',500)</script>")
 		return
 	}
 	md5S = append(md5S, cipherStr)
-	errCreateDir := os.MkdirAll(staticPath+"/"+folder1+"/"+folder2, 0755)
+	errCreateDir := os.MkdirAll(abDir, 0755)
 	if errCreateDir != nil {
 		beego.Info(errCreateDir)
 	}
-	resp, err2 := http.Get(domain + url)
-	if err2 != nil || resp.StatusCode != 200 {
-		c.Ctx.WriteString("error page")
-		return
+	if useOldPage {
+		fmt.Println("go create new")
+		go GetResponseBodyText(abPath, fullUrl)
+	} else {
+		fmt.Println("create new")
+		bodyText := GetResponseBodyText(abPath, fullUrl)
+		c.Ctx.WriteString(bodyText)
+	}
+	md5S = RemoveFromSlice(md5S, cipherStr)
+}
+
+func GetResponseBodyText(abPath string, fullUrl string) string {
+	resp, err := http.Get(fullUrl)
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("error page")
+		return ""
 	}
 	defer resp.Body.Close()
-	body, err3 := ioutil.ReadAll(resp.Body)
-	if err3 != nil {
-		c.Ctx.WriteString("body parse error")
-		return
-	}
-	if !useOldPage {
-		c.Ctx.WriteString(string(body))
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		fmt.Println("body parse error")
+		return ""
 	}
 	os.Remove(abPath)
 	newFile, errCreateFile := os.Create(abPath)
@@ -97,15 +102,14 @@ func (c *MainController) Get() {
 		beego.Info(errCreateFile)
 	}
 	beego.Info("写入文件：" + abPath + "    " + strconv.Itoa(n) + "字节")
-	md5S = RemoveFromSlice(md5S, cipherStr)
-	fmt.Println(1, md5S)
-	return
+	return string(body)
 }
 
-func isExist(filename string) bool {
+func IsExist(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil || os.IsExist(err)
 }
+
 func ContainsBySlice(md5S []string, s string) bool {
 	for _, v := range md5S {
 		if v == s {
@@ -126,4 +130,11 @@ func RemoveFromSlice(s []string, e string) []string {
 		s = append(s[:v], s[v+1:]...)
 	}
 	return s
+}
+
+func EncodeUrl(url string) string {
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(string(url)))
+	cipherEncode := md5Ctx.Sum(nil)
+	return hex.EncodeToString(cipherEncode)
 }
