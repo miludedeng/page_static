1d015c8e9049324dd6682da20a0fb3ed6aa1298c
miludedeng <383252209@qq.com>
2016-07-18 17:19:25 +0800

    rm loger set

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 	beego.BConfig.RunMode = config.Basic.RunMode
 	beego.BConfig.Listen.HTTPPort = config.Basic.HttpPort
 
-	beego.SetLogger("file", `{"filename":"logs/log"}`)
 	if beego.BConfig.RunMode == "prod" {
 		beego.BeeLogger.DelLogger("console")
 	}
